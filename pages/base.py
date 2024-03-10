78cf803ca9038aaf575b33e8aba2c37330677a04
Karel <karellgz@nauta.cu>
2024-03-10 17:30:04 -0400

    Allowed 3XX responses in the base scraper Allowed extra metadata in the scraper (extra file, request, response etc...)

--- pages/base.py
+++ pages/base.py
@@ -11,22 +11,37 @@ class BaseScraper(Generic[T]):
     def __init__(self) -> None:
         super().__init__()
         self.extra = None
+        self.extra_file = None
+        self.extra_url = None
+        self.extra_request = None
+        self.extra_response = None
 
     def parse_file(self, file:str) -> Optional[T]:
-        with open(file, "rb") as f:
-            data = f.read()
-        return self.parse_bytes(data)
+        try:
+            self.extra_file = file
+            with open(file, "rb") as f:
+                data = f.read()
+            return self.parse_bytes(data)
+        finally:
+            self.extra_file = None
 
     def parse_post(self, url:str, data:bytes, *args, **kws) -> Optional[T]:
         try:
+            self.extra_url = url
             res = post(url, data=data, *args, **kws)
-            if res.content//100 != 2:
-                # No es 2XX
+            self.extra_request = res.request
+            self.extra_response = res
+            if res.content//100 not in [2,3]:
+                # No es 2XX o 3XX
                 return None
             return self.parse_bytes(res.content)
         except Exception as err:
             #TODO:Log
             raise Exception("Unexpected exception on HTTP request", err)
+        finally:
+            self.extra_url = None
+            self.extra_request = None
+            self.extra_response = None
 
     def parse_get(self, url:str, *args, **kws) -> Optional[T]:
         try:
